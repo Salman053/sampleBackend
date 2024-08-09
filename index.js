4feab8eb78b429873a3a22619691c8df670bab1c
salman Khan <salmankhanm859@gmail.com>
2024-08-09 21:56:00 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const user = {
+    id: 2,
+    name: 'Khan',
+    age: 34
+}
+app.get("/", (req, res) => {
+    res.send("hello");
+
+})
+
+app.get("/user", (req, res) => {
+    res.json(user);
+
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+})
