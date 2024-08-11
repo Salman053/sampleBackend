ca8fc53e6143fb8aee78318459f68567f7052391
salman Khan <salmankhanm859@gmail.com>
2024-08-11 22:44:43 +0500

    working in models usermode.js and videomodel.js

---
+++ src/models/user.model.js
@@ -0,0 +1,55 @@
+import mongoose, { Schema, model } from "mongoose";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      unique: true,
+      minlength: 6,
+      maxlength: 20,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      unique: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudnary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudnary url
+      // required:true,
+      // default:''
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password must be provided"],
+      // minlength: 8,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+export const User = model("User", userSchema);
---
+++ src/models/video.model.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const videoSchema = new Schema({
+    videoFile:{
+        type: String, // cloudnary url
+        required: true
+    },
+    thumbnail:{
+        type: String, // cloudinary url
+        required: true
+    },
+    duration:{
+        type: Number,
+        required: true
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    views:{
+        type: Number,
+        default: 0
+    },
+    isPublished:{
+        type: Boolean,
+        default: false
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    likes:{
+        type: Number,
+        default: 0
+    }
+}, { timestamps: true });
+
+export const Video = model("Video", videoSchema);
